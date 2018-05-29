d57696ad7706a04def7ecd1c72d2264ac6d6883d
ackeri <h2ccoch3@gmail.com>
2018-05-29 15:52:30 -0700

    renamed diamond transaction to transactionmanager to avoid name conflict and improve code readability

---
+++ platform/includes/transactionmanager.h
@@ -0,0 +1,63 @@
+#ifndef TRANSACTION_MANAGER_H
+#define TRANSACTION_MANAGER_H
+
+#include "includes/data_types.h"
+#include <cstdarg>
+#include <event2/event.h>
+#include <event2/thread.h>
+#include <functional>
+#include <thread>
+
+namespace diamond {
+
+typedef std::function<void (void)> txn_function_t;
+typedef std::function<void (bool)> txn_callback_t;
+
+typedef struct txn_info {
+    txn_function_t func;
+    txn_callback_t callback;
+} txn_info_t;
+
+typedef struct reg_info {
+    txn_function_t func;
+    uint64_t reactive_id;
+} reg_info_t;
+
+class TxnManager {
+  public:
+    void Start();
+    int ExecuteTxn(txn_function_t func, txn_callback_t callback);
+    uint64_t ReactiveTxn(txn_function_t func);
+    void ReactiveStop(uint64_t reactive_id);
+
+  private:
+    event_base * txnEventBase;
+    std::thread * txnThread;
+    std::thread * reactiveThread;
+    std::vector<txn_function_t> reactiveList;
+    std::map<uint64_t, txn_function_t> funcMap;
+    uint64_t nextId = 0;
+
+    void startHelper();
+    void reactiveLoop();
+    uint64_t generateId();
+    uint64_t getNextReactiveTxn();
+    static void executeTxnCallback(evutil_socket_t fd, short what, void * arg);
+    static void signalCallback(evutil_socket_t fd, short what, void * arg);
+    static void registerCallback(evutil_socket_t fd, short what, void * arg);
+    static void reactiveStopCallback(evutil_socket_t fd, short what, void * arg);
+};
+
+extern TxnManager * txnManager;
+
+void StartTxnManager();
+int execute_txn(txn_function_t func, txn_callback_t callback);
+int execute_txn(txn_function_t func);
+uint64_t reactive_txn(txn_function_t func);
+
+void reactive_stop(uint64_t reactive_id);
+void abort_txn();
+
+}
+
+#endif
---
+++ platform/transactions/transactionmanager.cc
@@ -0,0 +1,171 @@
+#include "includes/transactions.h"
+
+#include <signal.h>
+#include "lib/message.h"
+
+namespace diamond {
+
+	txnManager = NULL;
+
+    void TxnManager::Start() {
+        evthread_use_pthreads();
+        txnEventBase = event_base_new();
+        evthread_make_base_notifiable(txnEventBase);
+
+        event * sigtermEvent = evsignal_new(txnEventBase,
+                                            SIGTERM,
+                                            signalCallback,
+                                            this);
+        event * sigintEvent = evsignal_new(txnEventBase,
+                                           SIGINT,
+                                           signalCallback,
+                                           this);
+
+        evsignal_add(sigtermEvent, NULL);
+        evsignal_add(sigintEvent, NULL);
+
+        txnThread = new std::thread(&TxnManager::startHelper, this);
+        reactiveThread = new std::thread(&TxnManager::reactiveLoop,
+                                         this);
+    }
+
+    uint64_t TxnManager::getNextReactiveTxn() {
+        while(true) {
+            sleep(1);
+        }
+        return 0;
+    }
+
+    void TxnManager::reactiveLoop() {
+        while(true) {
+            uint64_t reactive_id = DObject::GetNextNotification(true);
+            txn_function_t func = funcMap[reactive_id];
+            DObject::BeginReactive(reactive_id);
+            func();
+            DObject::TransactionCommit();
+        }
+    }
+
+    void TxnManager::startHelper() {
+        event_base_dispatch(txnEventBase);
+    }
+
+    void TxnManager::signalCallback(evutil_socket_t fd,
+                                    short what, void * arg) {
+        Debug("Transaction manager terminating on SIGTERM/SIGINT");
+        TxnManager *txnManager = (TxnManager *)arg;
+        event_base_loopbreak(txnManager->txnEventBase);
+    }
+
+    int TxnManager::ExecuteTxn(txn_function_t func,
+                               txn_callback_t callback) {
+        txn_info_t * info = new txn_info_t();
+        info->func = func;
+        info->callback = callback;
+        event * ev = event_new(txnEventBase, -1, 0, executeTxnCallback, info);
+        event_add(ev, NULL);
+        event_active(ev, 0, 1);
+        return 0;
+    }
+
+    void TxnManager::executeTxnCallback(evutil_socket_t fd,
+                                        short what, void * arg) {
+        txn_info_t * info = (txn_info_t *)arg;
+        DObject::TransactionBegin();
+        info->func();
+        bool committed = DObject::TransactionCommit();
+        info->callback(committed);
+        delete info;
+    }
+
+    //TODO: figure out how to handle duplicate registrations of the
+    //same function in C++
+    //TODO: recovery?
+    uint64_t TxnManager::ReactiveTxn(txn_function_t func) {
+        uint64_t reactive_id = generateId();
+        funcMap[reactive_id] = func;
+        reactiveList.push_back(func);
+
+        reg_info_t * info = new reg_info_t();
+        info->func = func;
+        info->reactive_id = reactive_id;
+        event * ev = event_new(txnEventBase, -1, 0,
+                               registerCallback, info);
+        event_add(ev, NULL);
+        event_active(ev, 0, 1);
+
+        return reactive_id;
+    }
+
+    void TxnManager::registerCallback(evutil_socket_t fd,
+                                      short what, void * arg) {
+        reg_info_t * info = (reg_info_t *)arg;
+        DObject::BeginReactive(info->reactive_id);
+        info->func();
+        int committed = DObject::TransactionCommit();
+        if (!committed) {
+            Panic("Reactive transaction did not commit");
+        }
+        delete info;
+    }
+
+    void TxnManager::ReactiveStop(uint64_t reactive_id) {
+        reg_info_t * info = new reg_info_t();
+        info->reactive_id = reactive_id;
+        event * ev = event_new(txnEventBase, -1, 0,
+                               reactiveStopCallback, info);
+        event_add(ev, NULL);
+        event_active(ev, 0, 1);
+    }
+
+    void TxnManager::reactiveStopCallback(evutil_socket_t fd,
+                                          short what, void * arg) {
+        reg_info_t * info = (reg_info_t *)arg;
+        DObject::Deregister(info->reactive_id);
+        delete info;
+    }
+
+    void StartTxnManager() {
+        if (txnManager == NULL) {
+            txnManager = new TxnManager();
+        }
+        txnManager->Start();
+    }
+
+    int execute_txn(txn_function_t func) {
+        int ret = execute_txn(func, [] (int committed) {});
+        return ret;
+    }
+
+    int execute_txn(txn_function_t func, txn_callback_t callback) {
+        if (txnManager == NULL) {
+            Panic("txnManager is null"); 
+        }
+        int ret = txnManager->ExecuteTxn(func, callback);
+        return ret;
+    }
+
+    uint64_t reactive_txn(txn_function_t func) {
+        if (txnManager == NULL) {
+            Panic("txnManager is null"); 
+        }
+        uint64_t reactive_id = txnManager->ReactiveTxn(func);
+        return reactive_id;
+    }
+
+    void reactive_stop(uint64_t reactive_id) {
+        if (txnManager == NULL) {
+            Panic("txnManager is null"); 
+        }
+        txnManager->ReactiveStop(reactive_id);
+    }
+    void abort_txn() {
+        //TODO: implement
+    }
+
+    uint64_t TxnManager::generateId() {
+        uint64_t result = nextId;
+        nextId++;
+        return result;
+    }
+}
