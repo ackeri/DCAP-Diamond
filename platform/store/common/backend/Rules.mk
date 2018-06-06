65a2aa8093b4812266962c0323f176f0401cefa2
ackeri <h2ccoch3@gmail.com>
2018-06-06 10:57:26 -0700

    removing duplicated functionality to link with tapir

--- platform/store/common/backend/Rules.mk
+++
@@ -1,8 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), \
-	commstore.cc kvstore.cc \
-	pubversionstore.cc versionstore.cc txnstore.cc)
-
-LIB-store-backend := $(o)kvstore.o $(o)txnstore.o $(o)versionstore.o \
-	$(o)commstore.o $(o)pubversionstore.o $(LIB-tcptransport) 
--- platform/store/common/frontend/Rules.mk
+++
@@ -1,7 +0,0 @@
-d := $(dir $(lastword $(MAKEFILE_LIST)))
-
-SRCS += $(addprefix $(d), \
-	asynccacheclient.cc cacheclient.cc)
-
-LIB-store-frontend := $(o)bufferclient.o
-
